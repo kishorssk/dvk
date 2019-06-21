ace717873758fb1c57baab3c8254a821ecf42ce7
kishorssk <kishor.sathigrama@gmail.com>
2019-06-21 12:26:32 +0530

    version update and jquery link enabled

--- application/views/header.php
+++ application/views/header.php
@@ -26,10 +26,10 @@
 
     <!-- Javascript calls
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
     <!-- <script src="<?=PUBLIC_URL?>js/jquery.columnizer.js"></script> -->
     
     <!-- CSS
@@ -37,9 +37,9 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
-    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/navbar.css?v=3.0">
+    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/navbar.css?v=4.0">
     <link rel="stylesheet" href="<?=PUBLIC_URL?>css/homepage.css?v=4.0">
-    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/fonts.css?v=3.0">
+    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/fonts.css?v=4.0">
     
     <!-- Fonts
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
